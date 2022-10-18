2334ecf52d47bccecbdd9745a5a405b2f59b1cbc
seb <sebastiandalknu@gmail.com>
2022-10-18 16:12:09 +0200

    openssl cringe

--- flake.nix
+++ flake.nix
@@ -1,11 +1,64 @@
 {
-  description = "A very basic flake";
-
-  outputs = { self, nixpkgs }: {
-
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
+  inputs = {
+    nixpkgs = {
+      url = "github:NixOS/nixpkgs/nixos-22.05";
+    };
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+    };
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "flake-utils";
+    };
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
   };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    rust-overlay,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      overlays = [(import rust-overlay)];
+      pkgs = import nixpkgs {inherit system overlays;};
+      rust = pkgs.rust-bin.nightly.latest.default;
+      platform = pkgs.makeRustPlatform {
+        cargo = rust;
+        rustc = rust;
+      };
+      shellInputs = with pkgs; [
+        clang
+        (rust.override {extensions = ["rust-src"];})
+      ];
+      appNativeBuildInputs = with pkgs; [
+        pkg-config
+      ];
+      appBuildInputs =
+        appRuntimeInputs
+        ++ (with pkgs; [
+          openssl
+        ]);
+      appRuntimeInputs = with pkgs; [
+      ];
+    in {
+      defaultPackage = platform.buildRustPackage {
+        src = ./.;
+        pname = "tts";
+        version = "0.1.0";
+        cargoLock.lockFile = ./Cargo.lock;
+        buildInputs = appBuildInputs;
+        nativeBuildInputs = appNativeBuildInputs;
+      };
+      devShell = pkgs.mkShell {
+        buildInputs = shellInputs ++ appBuildInputs;
+        nativeBuildInputs = appNativeBuildInputs;
+        shellHook = ''export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${pkgs.lib.makeLibraryPath appBuildInputs}"'';
+      };
+    });
 }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use reqwest::header::*;
+use reqwest;
 
 
 fn main() {
@@ -12,14 +12,14 @@ fn main() {
     // ?text_speaker={text_speaker}
     // &req_text={req_text}
 
-    let mut url =
-        "https://api22-normal-c-useast1a.tiktokv.com/media/api/text/speech/invoke/?text_speaker="
-        .push_str(voice).push_str("&req_text=").push_str(text)
-        .push_str("&speaker_map_type=0&aid=1233");
+    let url =
+        format!("https://api22-normal-c-useast1a.tiktokv.com/media/api/text/speech/invoke/?text_speaker={voice}&req_text={text}&speaker_map_type=0&aid=1233");
 
     let res = client
         .post(url)
         .header("session_id".to_string(), session_id)
-        .send()
-        .await?;
+        .send();
+
+    
+    // println!("this is res: {}", res);
 }
