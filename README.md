c2d34b2a925eb1f54a1839dc7c62b89c76440631
seb <sebastiandalknu@gmail.com>
2022-10-20 16:00:20 +0200

    lol

--- README.md
+++ README.md
@@ -1,6 +1,3 @@
-https://github.com/oscie57/tiktok-voice/wiki/Obtaining-SessionID
-
-1. Log in to the TikTok Web App
-2. Install the Cookie Editor extension for your browser.
-3. Open the extension and look for "sessionid"
-4. Copy it and paste it.
+SESSION_ID
+1. Log in to the TikTok Web App (in firefox for ezpz)
+2. Copy "session_id" cookie
--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,44 @@
-use reqwest;
-
+use std::fs::File;
+use std::io::Write;
+use serde_json::Value;
+use std::collections::HashMap;
+use reqwest::header::HeaderMap;
+use reqwest::header::{USER_AGENT, CONTENT_LENGTH, COOKIE};
 
 fn main() {
-    let client = reqwest::Client::new();
-    let session_id = "fecef076f71b6d2c3ffa3c85a1b74b2a".to_string();
-    let voice = "en_us_rocket".to_string();
-    let text ="testing string".to_string();
+    tts(
+        "fecef076f71b6d2c3ffa3c85a1b74b2a".to_string(),
+        "en_us_rocket".to_string(),
+        "this is a test".to_string()
+    )
+}
 
-    // build url
-    // "https://api22-normal-c-useast1a.tiktokv.com/media/api/text/speech/invoke/?text_speaker={text_speaker}&req_text={req_text}&speaker_map_type=0&aid=1233"
-    // ?text_speaker={text_speaker}
-    // &req_text={req_text}
+fn tts(session_id: String, voice: String, mut text: String) {
+    text = text.replace("+", "plus").replace(" ", "+").replace("&", "and");
+    let url = format!("https://api22-normal-c-useast1a.tiktokv.com/media/api/text/speech/invoke/?text_speaker={voice}&req_text={text}&speaker_map_type=0&aid=1233");
+    let mut headers = HeaderMap::new();
+        headers.insert(USER_AGENT, "com.zhiliaoapp.musically/2022600030 (Linux; U; Android 7.1.2; es_ES; SM-G988N; Build/NRD90M;tt-ok/3.12.13.1".parse().unwrap());
+        headers.insert(CONTENT_LENGTH, "0".parse().unwrap());
+    let cookie = format!("sessionid={}", &session_id);
+        headers.insert(COOKIE, cookie.parse().unwrap());
 
-    let url =
-        format!("https://api22-normal-c-useast1a.tiktokv.com/media/api/text/speech/invoke/?text_speaker={voice}&req_text={text}&speaker_map_type=0&aid=1233");
+    let res = reqwest::blocking::Client::new().post(url)
+        .headers(headers)
+        .send()
+        .expect("Oh no! #1")
+        .text()
+        .expect("Oh no! #2");
 
-    let res = client
-        .post(url)
-        .header("session_id".to_string(), session_id)
-        .send();
-
+    let map: HashMap<String, Value> = serde_json::from_str(&res).unwrap();
     
-    // println!("this is res: {}", res);
+    let test: String = map["data"]["v_str"].to_string()
+        .replace("String(", "")
+        .replace(")", "")
+        .replace('"', "");
+
+    let vstr = base64::decode(test).unwrap();
+
+    let mut file = File::create("output.mp3").expect("Oh no! #3");
+    file.write_all(&vstr).expect("Oh no! #4");
+
 }
