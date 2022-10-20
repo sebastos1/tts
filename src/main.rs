b1db2cf9339296e67e5ddf91973158df253c6efc
seb <sebastiandalknu@gmail.com>
2022-10-20 16:28:25 +0200

    l

--- src/main.rs
+++ src/main.rs
@@ -2,24 +2,23 @@ use std::fs::File;
 use std::io::Write;
 use serde_json::Value;
 use std::collections::HashMap;
-use reqwest::header::HeaderMap;
-use reqwest::header::{USER_AGENT, CONTENT_LENGTH, COOKIE};
+use reqwest::header::{HeaderMap, USER_AGENT, CONTENT_LENGTH, COOKIE};
 
 fn main() {
     tts(
-        "fecef076f71b6d2c3ffa3c85a1b74b2a".to_string(),
-        "en_us_rocket".to_string(),
-        "this is a test".to_string()
+        "fecef076f71b6d2c3ffa3c85a1b74b2a",
+        "en_male_m03_lobby",
+        "hello world"
     )
 }
 
-fn tts(session_id: String, voice: String, mut text: String) {
-    text = text.replace("+", "plus").replace(" ", "+").replace("&", "and");
+fn tts(session_id: &str, voice: &str, text: &str) {
+    let text = text.replace("+", "plus").replace(" ", "+").replace("&", "and");
     let url = format!("https://api22-normal-c-useast1a.tiktokv.com/media/api/text/speech/invoke/?text_speaker={voice}&req_text={text}&speaker_map_type=0&aid=1233");
     let mut headers = HeaderMap::new();
-        headers.insert(USER_AGENT, "com.zhiliaoapp.musically/2022600030 (Linux; U; Android 7.1.2; es_ES; SM-G988N; Build/NRD90M;tt-ok/3.12.13.1".parse().unwrap());
+        headers.insert(USER_AGENT, "com.zhiliaoapp.musically/2022600030 (Linux; U; Android 7.1.2; es_ES; SM-G988N; Build/NRD90M;tt-ok/3.12.13.1".parse().unwrap()); // not needed
         headers.insert(CONTENT_LENGTH, "0".parse().unwrap());
-    let cookie = format!("sessionid={}", &session_id);
+    let cookie = format!("sessionid={}", session_id);
         headers.insert(COOKIE, cookie.parse().unwrap());
 
     let res = reqwest::blocking::Client::new().post(url)
@@ -40,5 +39,4 @@ fn tts(session_id: String, voice: String, mut text: String) {
 
     let mut file = File::create("output.mp3").expect("Oh no! #3");
     file.write_all(&vstr).expect("Oh no! #4");
-
 }
